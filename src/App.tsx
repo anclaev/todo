03acefefd868889bac2da2cefcb52cf5d416a514
anclaev <iahugo@yandex.ru>
2021-04-30 16:38:50 +0400

    Настройка роутера.

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,19 @@
-import { useState } from 'react'
-import Board from './components/Board/Board'
+import { Route, Switch } from 'react-router'
 
-import Navbar from './components/Navbar/Navbar'
-import Todo from './components/Todo/Todo'
-import { ITodo } from './interfaces'
+import Navbar from './components/Navbar'
 
-const App: React.FC = () => {
-  const [TodoList, setTodoList] = useState<ITodo[]>([])
+import Chaos from './pages/Chaos'
+import Schemes from './pages/Schemes'
+import Todos from './pages/Todos'
 
-  const addHandler = (title: string) => {
-    const newTodo: ITodo = {
-      id: Date.now(),
-      title: title,
-      completed: false,
-    }
-
-    setTodoList((prev) => [newTodo, ...prev])
-  }
-
-  const toggleHandler = (id: number) =>
-    setTodoList((prev) =>
-      prev.map((todo) => {
-        if (todo.id === id) {
-          console.log('Last status: ', todo.completed)
-          let status = !todo.completed
-          console.log('New status: ', status)
-          todo.completed = status
-        }
-        return todo
-      })
-    )
-
-  const removeHandler = (id: number) => {
-    setTodoList((prev) => prev.filter((todo) => todo.id !== id))
-  }
-
-  return (
-    <>
-      <Navbar />
-      <Todo onAdd={addHandler} />
-      <Board
-        todos={TodoList}
-        onRemove={removeHandler}
-        onToggle={toggleHandler}
-      />
-    </>
-  )
-}
+const App: React.FC = () => (
+  <>
+    <Navbar />
+    <Switch>
+      <Route exact path="/" component={Todos} />
+      <Route path="/chaos" component={Chaos} />
+      <Route path="/schemes" component={Schemes} />
+    </Switch>
+  </>
+)
 export default App
