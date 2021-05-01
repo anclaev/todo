c8071822aab8bc477adcf910e0babbf899609608
anclaev <iahugo@yandex.ru>
2021-05-01 04:51:57 +0400

    Анимация названия задачи.

--- src/components/ETodo/index.tsx
+++ src/components/ETodo/index.tsx
@@ -1,20 +1,27 @@
 import s from './ETodo.module.sass'
 
+import { useState } from 'react'
+
 import { TTodo } from '../../types'
 import ECheck from '../ECheck'
 import Icon from '../EIcon'
 
 const ETodo: React.FC<TTodo> = (props) => {
+  const [focus, setFocus] = useState<boolean>(false)
+
   return (
     <div className={s.w}>
       <ECheck status={props.checked} switchStatus={props.switchStatus} />
-      <input
-        type="text"
-        className={s.t}
-        value={props.title}
-        onChange={(e) => props.changeTitle(e.target.value)}
-        placeholder="Задача"
-      />
+      <div className={`${s.t} ${focus ? s.tf : ''}`}>
+        <input
+          type="text"
+          value={props.title}
+          onChange={(e) => props.changeTitle(e.target.value)}
+          placeholder="Задача"
+          onFocus={() => setFocus(!focus)}
+          onBlur={() => setFocus(!focus)}
+        />
+      </div>
       <button className={s.i} onClick={() => props.removeHandler()}>
         <Icon name="close" />
       </button>
